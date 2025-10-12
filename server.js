ae655a85d7c27c75cf0fe6e0d3a2bface9cd1cc7
Asik76 <kartaevaslan10@gmail.com>
2025-10-12 15:06:19 +0600

    Added restore/delete archive system

--- server.js
+++ server.js
@@ -7,13 +7,13 @@ import { fileURLToPath } from "url";
 
 dotenv.config();
 const __filename = fileURLToPath(import.meta.url);
-const __dirname  = path.dirname(__filename);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "birthday-invite")));
 
-const TOKEN   = process.env.BOT_TOKEN;
+const TOKEN = process.env.BOT_TOKEN;
 const CHAT_ID = process.env.CHAT_ID;
 const DATA_FILE = "guests.json";
 const ARCHIVE_FILE = "deleted.json";
